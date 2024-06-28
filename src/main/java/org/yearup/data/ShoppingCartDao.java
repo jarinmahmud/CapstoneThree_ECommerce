1d87c1e1f42155716801aa040280a729d07ce224
Jarin Mahmud <jarinmahmud@gmail.com>
2024-06-28 05:04:06 -0400

    application properties updated

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,13 +1,11 @@
 package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
-import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
-    void addProductToCart(int userId, ShoppingCartItem item);
-    void updateProductInCart(int userId, int productId, int quantity);
-    void clearCart(int userId);
-    boolean contains(int userId, int productId);
+    void addItemToCart(int userId, int product_id);
+    int updateItemInCart(int userId, int product_id);
+    int deleteCart(int userId);
 }
